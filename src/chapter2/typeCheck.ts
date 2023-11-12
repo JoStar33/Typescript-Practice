3b5daab72f6cb6b4530d6ea9272819647bdb31ca
HoCG <rhwkd5012@naver.com>
2023-11-12 23:41:16 +0900

    feat: any타입과 타입 확장성에 대한 설명

--- src/chapter2/typeCheck.ts
+++ src/chapter2/typeCheck.ts
@@ -19,3 +19,27 @@ const calculateArea = (shape: Shape) => {
     return shape.width * shape.height;
   }
 };
+
+
+
+
+// 타입스크립트의 타입시스템은 항상 열려있음
+// 때문에 아래와 같은 당황스러운 결과를 개발자가 마주하기도함.
+interface Vector3D {
+  x: number;
+  y: number;
+  z: number;
+}
+
+const calculateLength1 = (v: Vector3D) => {
+  let length = 0;
+  for (const axis of Object.keys(v)) {
+    const coord = v[axis]; // >> 타입이 any로 반환됨.
+    length += Math.abs(coord);
+  }
+  return length;
+}
+
+// 이유는 아래와 같음. 
+const vec3D = { x: 3, y: 4, z: 1, address: 'hihi' };
+calculateLength1(vec3D); // 정상 NaN을 반환.
